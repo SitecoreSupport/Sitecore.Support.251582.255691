ab2bd7e53f455b5324ff24e7cf3d7b69debcca38
DK\IEKA <ieka@sitecore.net>
2018-09-26 12:26:08 +0300

    Cumulative patch for issues #251582 and #255691

---
+++ src/Sitecore.Support.251582.255691/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.251582.255691")]
+[assembly: AssemblyProduct("Sitecore.Support.251582.255691")]
+[assembly: ComVisible(false)]
